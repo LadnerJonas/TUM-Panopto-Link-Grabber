66575d639524330d171ca3d5e50a95940ad5e9b6
Jonas Ladner <jonas.ladner@tum.de>
2021-12-28 23:12:05 +0100

    Combine download and audio extract command

--- popup.js
+++ popup.js
@@ -21,25 +21,19 @@ function copyFFMPEGDownloadCommand() {
                 if (!("playlists" in response)) {
                     return;
                 }
-                let ffmpegCommand = "ffmpeg -i " + response.playlists + " -c copy video.mp4"
+                let ffmpegCommand = "ffmpeg -i " + response.playlists + " -c copy video.mp4 && ffmpeg -i video.mp4 -acodec copy audio.aac"
                 navigator.clipboard.writeText(ffmpegCommand);
             }
         );
     });
 }
-function copyFFMPEGAudioExtractCommand() {
-    navigator.clipboard.writeText("ffmpeg -i video.mp4 -acodec copy original-audio.aac");
-}
 function copyFFMPEGCombineCommand() {
-    navigator.clipboard.writeText("ffmpeg -i video.mp4 -i audio.mp3 -vcodec copy -acodec copy -map 0:0 -map 1:0 denoised-video.mp4");
+    navigator.clipboard.writeText("ffmpeg -i video.mp4 -i audio.mp3 -vcodec copy -acodec copy -map 0:0 -map 1:0 video-improved.mp4");
 }
-
-
 
 window.onload = function () {
     document.getElementById("copy-url").onclick = copyUrl;
-    document.getElementById("copy-download").onclick = copyFFMPEGDownloadCommand;
-    document.getElementById("copy-audio").onclick = copyFFMPEGAudioExtractCommand;
+    document.getElementById("copy-download-and-audio-extract").onclick = copyFFMPEGDownloadCommand;
     document.getElementById("copy-combine").onclick = copyFFMPEGCombineCommand;
 };
 
